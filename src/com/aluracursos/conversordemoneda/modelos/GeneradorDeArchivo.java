896c2c42643a3190c09d7c632f56f7131e33be35
Gaston Lopez <gastonlopez436@gmail.com>
2024-10-14 02:08:23 -0300

    add generador de json-

--- src/com/aluracursos/conversordemoneda/modelos/GeneradorDeArchivo.java
+++ src/com/aluracursos/conversordemoneda/modelos/GeneradorDeArchivo.java
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class GeneradorDeArchivo {
     public void guardarJson(TipoDeCambio tipoDeCambio) throws IOException {
